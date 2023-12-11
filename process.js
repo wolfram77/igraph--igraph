d8d4f9f7eb39ac07a5a115648b415813753dd89d
Subhajit Sahu <wolfram77@gmail.com>
2023-12-11 14:21:06 +0530

    :bug: process output logs

--- process.js
+++ process.js
@@ -6,6 +6,18 @@ const readline = require('readline');
 
 
 
+//#region CONSTANTS
+// ----------------
+
+const RGRAPH = /^Reading graph from .*\/(.*?)\.mtx\.el/m;
+const RORDER = /^Read graph with (\d+) vertices and (\d+) edges\./m;
+const RMODUL = /^(\d+) communities found; modularity score is (.+?)\.$/m;
+const RRESLT = /^Duration: (.+?) ms/m;
+//#endregion
+
+
+
+
 //#region FILE
 // -----------
 
@@ -19,6 +31,69 @@ function readFile(pth) {
 function writeFile(pth, d) {
   d = d.replace(/\r?\n/g, os.EOL);
   fs.writeFileSync(pth, d);
+}
+
+// Write rows to CSV file.
+function writeCsv(pth, rows) {
+  var cols = Object.keys(rows[0]);
+  var a = cols.join()+'\n';
+  for (var r of rows)
+    a += [...Object.values(r)].map(v => `"${v}"`).join()+'\n';
+  writeFile(pth, a);
+}
+//#endregion
+
+
+
+
+//#region PROCESS LOGS
+// -------------------
+
+// Read/parse a line from log file.
+function readLogLine(ln, data, state) {
+  state = state || {};
+  ln = ln.replace(/^\d+-\d+-\d+ \d+:\d+:\d+\s+/, '');
+  if (RGRAPH.test(ln)) {
+    var [, graph] = RGRAPH.exec(ln);
+    if (!data.has(graph)) data.set(graph, []);
+    state.graph = graph;
+  }
+  else if (RORDER.test(ln)) {
+    var [, order, size] = RORDER.exec(ln);
+    state.order = parseFloat(order);
+    state.size  = parseFloat(size);
+  }
+  else if (RMODUL.test(ln)) {
+    var [, communities_count, modularity] = RMODUL.exec(ln);
+    state.communities_count = parseFloat(communities_count);
+    state.modularity        = parseFloat(modularity);
+  }
+  else if (RRESLT.test(ln)) {
+    var [, duration] = RRESLT.exec(ln);
+    data.get(state.graph).push(Object.assign({}, state, {
+      duration: parseFloat(duration),
+    }));
+  }
+  return state;
+}
+
+// Read/parse log file.
+function readLog(pth) {
+  var text  = readFile(pth);
+  var lines = text.split('\n');
+  var data  = new Map();
+  var state = null;
+  for (var ln of lines)
+    state = readLogLine(ln, data, state);
+  return data;
+}
+
+// Process CSV data.
+function processCsv(data) {
+  var a = [];
+  for (var rows of data.values())
+    a.push(...rows);
+  return a;
 }
 //#endregion
 
@@ -38,6 +113,7 @@ async function headerLines(pth) {
   }
   return a+1;  // +1 for the row/column count line
 }
+//#endregion
 
 
 
@@ -46,7 +122,17 @@ async function headerLines(pth) {
 // -----------
 
 async function main(cmd, inp, out) {
+  var data = cmd.startsWith('csv')? readLog(inp) : null;
+  if (path.extname(out)==='') cmd += '-dir';
   switch (cmd) {
+    case 'csv':
+      var rows = processCsv(data);
+      writeCsv(out, rows);
+      break;
+    case 'csv-dir':
+      for (var [graph, rows] of data)
+        writeCsv(path.join(out, graph+'.csv'), rows);
+      break;
     case 'header-lines':
       var lines = await headerLines(inp);
       console.log(lines);
@@ -57,3 +143,4 @@ async function main(cmd, inp, out) {
   }
 }
 main(...process.argv.slice(2));
+//#endregion
